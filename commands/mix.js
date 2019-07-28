fca35bb330011466a3d8420d43effe145a734639
greysdawn <greysdawn@gmail.com>
2019-07-28 18:48:01 -0400

    Add new commands

---
+++ commands/mix.js
@@ -0,0 +1,6 @@
+module.exports = {
+	help: "Mix two colors",
+	usage: [" [color1] [color2] - Mixes two colors and gives the result. NOTE: currentl only accepts rgb/hsv values without spaces! eg: rgb(0,0,0)"],
+	examples: ["s!m red blue", "s!m rgb(0,0,0) red", "s!mix aa55aa blue"],
+	alias: ['m', 'blend']
+}
