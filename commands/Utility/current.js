8a98d02ba1acee67a9b340a1801eaa20eb76db83
xgreyskiesx <greysdawn@gmail.com>
2020-03-30 11:01:25 -0400

    Pad colors for s!curr command

--- commands/Utility/current.js
+++ commands/Utility/current.js
@@ -5,7 +5,10 @@ module.exports = {
 		var role = await bot.utils.getRawUserRole(bot, msg.guild, msg.member);
 		if(!role) return "Either you don't have a color role or I couldn't get it :(";
 
-		var color = bot.tc(role.color.toString(16));
+		var conv = role.color.toString(16);
+		if(conv.length < 6) while(conv.length < 6) conv = "0"+conv; //number padding
+		var color = bot.tc(conv);
+
 		return {
 			embed: {
 				title: "Current color: "+color.toHexString().toUpperCase(),
