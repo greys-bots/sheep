0f70dc442596b4d56b81d69b876517bc150c6ecb
xgreyskiesx <greysdawn@gmail.com>
2020-05-17 20:33:31 -0400

    Fix config creation when toggling to server-based colors

--- commands/Admin/toggle.js
+++ commands/Admin/toggle.js
@@ -1,11 +1,13 @@
 module.exports = {
 	help: ()=> "Toggle between user-based or server-based color roles",
 	usage: ()=> [" - toggles the color role mode"],
-	execute: async (bot, msg, args, config = {role_mode: 0}) => {
-		var mode = Math.abs(config.role_mode - 1);
+	execute: async (bot, msg, args) => {
+		var config = (await bot.stores.configs.get(msg.guild.id)) || {};
+		var mode = Math.abs((config.role_mode || 0) - 1);
 		if(isNaN(mode)) mode = 0;
 		try {
-			await bot.stores.configs.update(msg.guild.id, {role_mode: mode});
+			if(config) await bot.stores.configs.update(msg.guild.id, {role_mode: mode});
+			else await bot.stores.configs.create(msg.guild.id, {role_mode: mode});
 		} catch(e) {
 			return "ERR:"+e;
 		}
