bf598852052d3667b299613acc4ed96c0ed21b47
xgreyskiesx <greysdawn@gmail.com>
2020-05-17 20:37:37 -0400

    Whoops

--- commands/Admin/toggle.js
+++ commands/Admin/toggle.js
@@ -2,8 +2,8 @@ module.exports = {
 	help: ()=> "Toggle between user-based or server-based color roles",
 	usage: ()=> [" - toggles the color role mode"],
 	execute: async (bot, msg, args) => {
-		var config = (await bot.stores.configs.get(msg.guild.id)) || {};
-		var mode = Math.abs((config.role_mode || 0) - 1);
+		var config = await bot.stores.configs.get(msg.guild.id); //don't resolve it to an empty object, actually,
+		var mode = Math.abs((config && config.role_mode ? config.role_mode : 0) - 1);
 		if(isNaN(mode)) mode = 0;
 		try {
 			if(config) await bot.stores.configs.update(msg.guild.id, {role_mode: mode});
