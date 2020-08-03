4ba999e8aa75318d08b013fc0d4c543a69915370
greysdawn <greysdawn@gmail.com>
2020-08-03 18:52:19 -0400

    Fix role grabbing when users leave servers

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -2,7 +2,7 @@ module.exports = async (member, bot)=> {
 	var role = await bot.stores.userRoles.getRaw(member.guild.id, member.id);
 	if(!role) return;
 	try {
-		var rl = member.guild.roles.cache.find(role.role_id);
+		var rl = member.guild.roles.resolve(role.role_id);
 		if(rl) role.delete("Member left server");
 	} catch(e) {
 		console.log(e);
