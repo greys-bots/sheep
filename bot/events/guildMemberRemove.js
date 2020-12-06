7cd7476441e4ab6f8ab7585a36fe6e1832673c1c
greysdawn <greysdawn@gmail.com>
2020-12-06 15:55:09 -0500

    Fix role deletion

--- bot/events/guildMemberRemove.js
+++ bot/events/guildMemberRemove.js
@@ -2,7 +2,7 @@ module.exports = async (member, bot)=> {
 	var role = await bot.stores.userRoles.getRaw(member.guild.id, member.id);
 	if(!role) return;
 	try {
-		var rl = member.guild.roles.cache.find(role.role_id);
+		var rl = await member.guild.roles.fetch(role.role_id);
 		if(rl) role.delete("Member left server");
 	} catch(e) {
 		console.log(e);
