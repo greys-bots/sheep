5066fdcd89e64f309a3ecacba1ec205e728e03f3
greysdawn <greysdawn@gmail.com>
2021-11-18 19:34:37 -0500

    Fix about embed in slash command
    Fixes #10

--- slashcommands/about.js
+++ slashcommands/about.js
@@ -9,13 +9,13 @@ module.exports = {
 	async execute(ctx) {
 		return {embeds: [{
 			title: '**About**',
-			description: "Eee! I'm Fox! I help people set up forms and responses here on Discord!",
+			description: "Baa! I'm Sheep! I help people change their name color here on Discord.\nMy prefixes are `s!`, `sh!`, `sheep!`, and `baa!`",
 			fields:[
 				{name: "Creators", value: "[greysdawn](https://github.com/greysdawn) | (GS)#6969"},
-				{name: "Invite", value: `[Clicky!](${process.env.INVITE})`,inline: true},
+				{name: "Invite", value: "[Clicky!](https://discordapp.com/api/oauth2/authorize?client_id=585271178180952064&permissions=268462080&scope=bot)",inline: true},
 				{name: "Support Server", value: "[Clicky!](https://discord.gg/EvDmXGt)", inline: true},
-				{name: "Other Links", value: "[Repo](https://github.com/greys-bots/form-fox)"},
-				{name: "Stats", value: `Guilds: ${ctx.client.guilds.cache.size} | Users: ${ctx.client.users.cache.size}`},
+				{name: "Other Links", value: "[Repo](https://github.com/greys-bots/sheep) | [Website](https://sheep.greysdawn.com/)"},
+				{name: "Stats", value: `Guilds: ${guilds} | Users: ${users}`},
 				{name: "Support my creators!", value: "[Ko-Fi](https://ko-fi.com/greysdawn) | [Patreon](https://patreon.com/greysdawn)"}
 			]
 		}]}
