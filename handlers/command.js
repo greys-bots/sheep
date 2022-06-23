355aa244fce0e355d2ca52b95da6f1d375a49343
greysdawn <greysdawn@gmail.com>
2022-06-23 12:50:31 -0400

    Add text command warnings

--- handlers/command.js
+++ handlers/command.js
@@ -2,6 +2,7 @@ const { Collection } = require('discord.js');
 
 class CommandHandler {
 	cooldowns = new Map();
+	warnings = new Set();
 
 	constructor(bot) {
 		this.bot = bot;
@@ -101,6 +102,10 @@ class CommandHandler {
 
 		try {
 			var res = await command.execute(this.bot, msg, args, cfg);
+			await this.handleWarning({
+				user: msg.author,
+				channel: msg.channel
+			})
 		} catch(e) {
 			return Promise.reject(e);
 		}
@@ -181,6 +186,19 @@ class CommandHandler {
 			})
 		}
 		return command;
+	}
+
+	async handleWarning(ctx) {
+		if(!process.env.COMMAND_WARN) return;
+		if(this.warnings.has(ctx.user.id)) return;
+		this.warnings.add(ctx.user.id);
+		setTimeout(() => this.warnings.delete(ctx.user.id), 1000 * 60 * 60 * 6) // show warning again after 6 hours
+		await ctx.channel.send(
+			`⚠️ **Warning:** ⚠️\n` +
+			`Text commands will be removed soon! ` +
+			`For more information, please see this post: ` +
+			`<https://www.patreon.com/posts/68150511>`
+		)
 	}
 }
 
